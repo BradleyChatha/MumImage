027e3b59997183357433e593cf9f0d65773b991d
Bradley Chatha <bradley@chatha.dev>
2021-09-25 10:39:00 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,128 @@
+open System
+open SdlangSharp
+open Microsoft.Data.Sqlite
+open System.IO
+open System.Text.RegularExpressions
+open System.Diagnostics
+
+let getDb =
+    let conn = new SqliteConnection("Data Source=index.db")
+    conn.Open ()
+
+    let command = conn.CreateCommand ()
+    command.CommandText <- @"
+        CREATE TABLE IF NOT EXISTS files (
+            keyword TEXT NOT NULL,
+            path TEXT NOT NULL
+        );
+        CREATE INDEX IF NOT EXISTS idx_files_keyword ON files (keyword);
+        CREATE UNIQUE INDEX IF NOT EXISTS idx_files_unique ON files (keyword, path);
+    "
+    command.ExecuteNonQuery () |> ignore
+
+    conn
+
+let keywordRegex = Regex(@"([a-zA-Z0-9]+)")
+
+type InsertCommand = {
+    Com : SqliteCommand
+    Keyword : SqliteParameter
+    Path : SqliteParameter
+}
+
+let getInsertCommand (db : SqliteConnection) =
+    let com = db.CreateCommand ()
+    com.CommandText <- @"
+        INSERT OR IGNORE INTO files (keyword, path) VALUES ($keyword, $path);
+    "
+    {
+        Com = com
+        Keyword = com.Parameters.Add ("$keyword", SqliteType.Text)
+        Path = com.Parameters.Add ("$path", SqliteType.Text)
+    }
+
+let getKeywords filename =
+    keywordRegex.Matches filename 
+    |> Seq.filter (fun m -> m.Success)
+    |> Seq.map (fun m -> m.Captures.[0].Value)
+
+let rec rebuildIndex db dir =
+    for subdir in (Directory.EnumerateDirectories dir) do
+        rebuildIndex db subdir
+
+    let insertCommand = getInsertCommand db
+    for file in (Directory.EnumerateFiles dir) do
+        for keyword in (getKeywords (Path.GetFileName file)) do
+            insertCommand.Keyword.Value <- keyword
+            insertCommand.Path.Value <- file
+            insertCommand.Com.ExecuteNonQuery () |> ignore
+
+let getSdl =
+    if File.Exists ("./cats.sdl") then
+        let text = File.ReadAllText("./cats.sdl")
+        let reader = SdlReader(text.AsSpan())
+        let sdl = reader.ToAst()
+        let map = Collections.Generic.List<string * string>()
+        for child in sdl.Children do
+            for value in child.Values do
+                map.Add((child.Name, value.String))
+        map
+    else
+        Collections.Generic.List<string * string>()
+
+let doReflect (db : SqliteConnection) =
+    let command = db.CreateCommand ()
+    command.CommandText <- @"
+        SELECT * FROM files;
+    "
+
+    if not (Directory.Exists (Path.Combine ("reflected"))) then
+        Directory.CreateDirectory "reflected" |> ignore
+    if not (Directory.Exists (Path.Combine ("reflected", "keywords"))) then
+        Directory.CreateDirectory "reflected/keywords" |> ignore
+
+    let reader = command.ExecuteReader ()
+    let sdl    = getSdl
+    while reader.Read() do
+        let keyword = reader.GetString 0
+        let path    = reader.GetString 1
+        let dir     = Path.Combine ("reflected", "keywords", keyword)
+        let file    = Path.Combine (dir, Path.GetFileName path)
+        if not (Directory.Exists dir) then
+            Directory.CreateDirectory dir |> ignore
+
+        // This is just for mum, no need for it to be perfect code.
+        for (cat, _) in (sdl |> Seq.filter (fun (_, key) -> key = keyword)) do
+            let catDir = Path.Combine ("reflected", "categories", cat)
+            let catFile = Path.Combine (catDir, Path.GetFileName path)
+            if not (Directory.Exists catDir) then
+                Directory.CreateDirectory catDir |> ignore
+            if OperatingSystem.IsWindows () then
+                (Process.Start ("cmd.exe", ["/C"; "mklink"; catFile; path])).WaitForExit()
+            elif OperatingSystem.IsLinux () then
+                Process.Start ("/bin/bash", ["-c"; $"ln -s '{path}' '{catFile}'"]) |> ignore
+            else
+                failwith "Unsupported operating system."
+
+        if OperatingSystem.IsWindows () then
+            (Process.Start ("cmd.exe", ["/C"; "mklink"; file; path])).WaitForExit()
+        elif OperatingSystem.IsLinux () then
+            Process.Start ("/bin/bash", ["-c"; $"ln -s '{path}' '{file}'"]) |> ignore
+        else
+            failwith "Unsupported operating system."
+
+[<EntryPoint>]
+let main argv =
+    let command = if argv.Length < 1 then "<none given>" else argv.[0]
+    let path = if argv.Length < 2 then "./" else argv.[1]
+
+    let db = getDb
+    match command with
+    | "index" ->
+        let transaction = db.BeginTransaction ()
+        rebuildIndex db path
+        transaction.Commit ()
+    | "reflect" ->
+        doReflect db
+    | _ -> failwith $"Unknown command '{command}'"
+    0
